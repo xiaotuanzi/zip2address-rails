7f89f4673d923f89444ef27ca19cb28d04ccd28e
AKIYOSHI, Masamichi <akiyoshi.rigaku@gmail.com>
2013-10-02 21:34:13 +0900

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zip2addressRails::Application.config.secret_token = 'b0aaa7b2c3ef8e2993bc10ce0e6f06aaee75f9b9829e6bbdee07614d80e2245c353a4098d66916c2921d53d3170d9e787f6cf8120835191454bbc5f68bb131bb'
